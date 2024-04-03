ae404d869baf4193dcf3867060a91fcfed088d17
jasin1 <jasin.tairaidrissi@gmail.com>
2024-04-03 08:23:00 +0200

    practicing with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import EmailInput from './assets/components/EmailInput/EmailInput.jsx'
-import './App.css'
+import { useState } from 'react';
+import EmailInput from './assets/components/EmailInput/EmailInput.jsx';
+import './App.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
